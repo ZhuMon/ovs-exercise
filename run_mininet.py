0e47fc492c0a4a778c2ee4525c99fa767d3d109c
ZhuMon <richardlin0212@gmail.com>
2020-12-23 19:06:07 +0800

    Simplify topology & Add controller
    Use ryu.app.simple_switch_13 as controller
    Change topology from triangle to only one switch
    Change topology format, you should define each switch port number

--- run_mininet.py
+++ run_mininet.py
@@ -3,7 +3,8 @@ import os, sys, json, argparse
 from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.cli import CLI
-from mininet.node import OVSKernelSwitch, Ryu
+from mininet.node import OVSKernelSwitch, Ryu, RemoteController
+from simple_switch_13 import SimpleSwitch13
 
 
 class MyTopo(Topo):
@@ -12,7 +13,6 @@ class MyTopo(Topo):
 
         with open(topo_file, 'r') as f:
             topo = json.load(f)
-
 
         hosts = topo['hosts']
         switches = topo['switches']
@@ -39,16 +39,29 @@ class MyTopo(Topo):
 
         for link in host_links:
             host_name = link['node1']
-            host_sw   = link['node2']
+            sw_name, sw_port = self.parse_switch_node(link['node2'])
             host_num = int(host_name[1:])
-            sw_num   = int(host_sw[1:])
+            sw_num   = int(sw_name[1:])
 
-            self.addHost(host_name)
-            self.addLink(host_name, host_sw)
+            self.addHost(host_name, ip = '10.0.%02d.%02d' % (sw_num, host_num), mac='08:00:00:00:%02d:%02d' % (sw_num, host_num))
+            self.addLink(host_name, sw_name, port1 = 1, port2=sw_port)
 
         for link in switch_links:
-            self.addLink(link['node1'], link['node2'])
+            sw1_name, sw1_port = self.parse_switch_node(link['node1'])
+            sw2_name, sw2_port = self.parse_switch_node(link['node2'])
+            self.addLink(sw1_name, sw2_name,
+                    port1=sw1_port, port2=sw2_port)
+
     
+    def parse_switch_node(self, node):
+        assert(len(node.split('-')) == 2)
+        sw_name, sw_port = node.split('-')
+        try:
+            sw_port = int(sw_port[1:])
+        except:
+            raise Exception('Invalid switch node in topology file: {}'.formate(node))
+        return sw_name, sw_port
+
     def parse_links(self, unparsed_links):
         links = []
         for link in unparsed_links:
@@ -68,13 +81,16 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--topo', help = 'Path to topology json',
                         type=str, required=False, default='./topology.json')
+    parser.add_argument('-c', '--controller', help = 'Path to controller',
+                        type=str, required=False, default='./simple_switch_13.py')
+
 
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = get_args()
     topo = MyTopo(args.topo)
-    net = Mininet(topo, OVSKernelSwitch, controller=Ryu("Ryu_controller"))
+    net = Mininet(topo, switch=OVSKernelSwitch, controller=Ryu("c0", args.controller))
     net.start()
     for h in net.hosts:
         print("**********")
@@ -82,6 +98,7 @@ if __name__ == "__main__":
         print("default interface: %s\t%s\t%s" % (h.defaultIntf().name, h.defaultIntf().IP(), h.defaultIntf().MAC()))
         print("**********")
 
+    
     CLI(net)
 
     net.stop()
